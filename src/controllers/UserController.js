6131f780e64aa7bc7b463440c770e39635650fdd
Igor Bedon de Sousa <igorbedon@gmail.com>
2025-01-14 00:04:13 -0300

    Adding basic User CRUD structure

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,11 +1,68 @@
+import { where } from "sequelize";
 import User from "../models/UserModel";
 
 class UserController {
+
+  async index(req, res) {
+    try{
+      const users = await User.findAll();
+      if(!users) return res.json(null)
+      return res.json(users);
+    } catch(e) {
+      res.status(400).json({
+        "errors": e.errors.map((erro) => erro.message)
+      })
+    }
+  }
+
   async create(req, res) {
     try {
-      const novoUser = await User.create(req.body)
+      const novoUser = await User.create(req.body);
       res.json(novoUser);
     } catch(e) {
+      res.status(400).json({
+        "errors": e.errors.map((erro) => erro.message)
+      })
+    }
+  }
+
+  async read(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if(!user) return res.json(null);
+      return res.json(user);
+    } catch(e) {
+      res.status(400).json({
+        "errors": e.errors.map((erro) => erro.message)
+      })
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if(!user) return res.json("Usuário não encontrado");
+
+      const newUser = await user.update(req.body);
+      return res.json(newUser);
+
+    } catch(e) {
+      console.log(e);
+      res.status(400).json({
+        "errors": e.errors.map((erro) => erro.message)
+      })
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if(!user) return res.json("Usuário não encontrado");
+
+      await user.destroy();
+      res.json(user);
+    } catch(e) {
+      console.log(e);
       res.status(400).json({
         "errors": e.errors.map((erro) => erro.message)
       })
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,10 @@ import userController from '../controllers/UserController';
 
 const router = new Router();
 
+router.get('/', userController.index);
 router.post('/', userController.create);
+router.get('/:id', userController.read);
+router.put('/update/:id', userController.update);
+router.delete('/delete/:id', userController.delete);
 
 export default router;
